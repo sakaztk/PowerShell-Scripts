7924d5e3f5a487536e7d15ee20ef87f527026c55
Kazutaka Sakoda <kazutaka.sakoda@gmail.com>
2023-06-30 18:27:07 +0900

    Update Install-ChromeDriverBinary.psm1

--- PythonOnWindows/functions/Install-ChromeDriverBinary.psm1
+++ PythonOnWindows/functions/Install-ChromeDriverBinary.psm1
@@ -1,8 +1,15 @@
 function Install-ChromeDriverBinary {
+    [CmdletBinding()]
+    Param (
+        [String]$PipCommand = 'pip'
+    )
     $chromeVer = (Get-Item (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe').'(Default)').VersionInfo.ProductVersion
     $chromeBuild = $chromeVer -replace '(\d*\.\d*\.\d*)\..*','$1'
     $content = Invoke-WebRequest -uri "https://chromedriver.chromium.org/downloads" -UseBasicParsing
     $driverLatestUri = ($content.links.href | Select-String -Pattern "path=$chromeBuild" | Get-Unique | Sort-Object -Descending)[0]
     $driverVer = $driverLatestUri -replace ".*($chromeBuild.\d*).*",'$1'
-    pip install chromedriver-binary==$driverVer
+    Write-Verbose "Current Chrome Version: $chromeVer"
+    Write-Verbose "Download Driver Version: $driverVer"
+    Write-Verbose "Invoking Command: $PipCommand install chromedriver-binary==$driverVer"
+    . $PipCommand install chromedriver-binary==$driverVer
 }
