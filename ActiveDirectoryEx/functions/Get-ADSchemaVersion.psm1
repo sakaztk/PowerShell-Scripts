d487010ee4a33bb813e03553d10ea5ece98233a2
Kazutaka Sakoda <kazutaka.sakoda@gmail.com>
2024-04-21 17:39:04 +0900

    Create ActiveDirectoryEx

---
+++ ActiveDirectoryEx/functions/Get-ADSchemaVersion.psm1
@@ -0,0 +1,15 @@
+﻿function Get-ADSchemaVersion {
+    [PSCustomObject][Ordered]@{
+        'Windows Server 2000' = 13
+        'Windows Server 2003' = 30
+        'Windows Server 2003 R2' = 31
+        'Windows Server 2008' = 44
+        'Windows Server 2008 R2' = 47
+        'Windows Server 2012' = 56
+        'Windows Server 2012 R2' = 69
+        'Windows Server 2016' = 87
+        'Windows Server 2019' = 88
+        'Windows Server 2022' = 98
+        'Current' = (Get-ADObject (Get-ADRootDSE).SchemaNamingContext -Property ObjectVersion).ObjectVersion
+    }
+}
