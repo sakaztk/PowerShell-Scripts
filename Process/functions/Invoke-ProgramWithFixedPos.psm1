89d1ee24b9d6d7d0fbc042362dbb131f06e6cfd3
Kazutaka Sakoda <kazutaka.sakoda@gmail.com>
2025-01-23 19:38:23 +0900

    Add Process

---
+++ Process/functions/Invoke-ProgramWithFixedPos.psm1
@@ -0,0 +1,19 @@
+﻿Function Invoke-ProgramWithFixedPos
+{
+    Param(
+        [String]$Program,
+        [Int]$X,
+        [Int]$Y
+    )
+    Begin{
+    Add-Type -Namespace Imports -Name User32 @'
+[DllImport("user32.dll")]public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+'@
+    }
+    Process
+    {
+        $process = [System.Diagnostics.Process]::Start($Program)
+        $process.WaitForInputIdle() | Out-Null
+        [Imports.User32]::SetWindowPos($process.MainWindowHandle, 0, $X, $Y, 0, 0, 1) | Out-Null
+    }
+}
