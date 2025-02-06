f548df4e584dbfb7633850e2e3c22c8c192b2851
Kazutaka Sakoda <kazutaka.sakoda@gmail.com>
2025-02-07 07:35:05 +0900

    Create Set-TaskbarViewButton.psm1

---
+++ WindowsSettings/functions/Set-TaskbarViewButton.psm1
@@ -0,0 +1,17 @@
+function Set-TaskbarViewButton {
+    param (
+        [Parameter(Mandatory,ValueFromPipeline,Position=0)]
+        [ValidateSet('Hide', 'Show')]
+        [String]$State
+    )
+    Begin {
+        $key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+    }
+    Process {
+        switch ($State) {
+            "Hide" {$value = 0}
+            "Show" {$value = 1}
+        }
+        Set-ItemProperty -Path $key -Name ShowTaskViewButton -Value $value
+    }
+}
