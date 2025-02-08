4522ef062b98e3378264bfe9a8b6d90fef39f8a5
Kazutaka Sakoda <kazutaka.sakoda@gmail.com>
2025-02-08 09:01:22 +0900

    Create Set-TaskbarSearch.psm1

---
+++ WindowsSettings/functions/Set-TaskbarSearch.psm1
@@ -0,0 +1,18 @@
+function Set-TaskbarSearch {
+    param (
+        [Parameter(Mandatory,ValueFromPipeline,Position=0)]
+        [ValidateSet('Hide', 'Icon', 'Box')]
+        [String]$Mode
+    )
+    Begin {
+        $key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search'
+    }
+    Process {
+        switch ($Mode) {
+            "Hide" {$value = 0}
+            "Icon" {$value = 1}
+            "Box" {$value = 2}
+        }
+        Set-ItemProperty -Path $key -Name SearchboxTaskbarMode -Value $value
+    }
+}
